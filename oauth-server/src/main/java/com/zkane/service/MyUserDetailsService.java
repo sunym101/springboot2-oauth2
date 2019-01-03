7a5e818ef74eac80f3c45ff56d85379154b4f037
sunyongmeng <sunyongmeng@LAPTOP-1C2EREA1>
2019-01-03 12:02:01 +0800

    改为DB里存储用户信息

---
+++ oauth-server/src/main/java/com/zkane/service/MyUserDetailsService.java
@@ -0,0 +1,49 @@
+package com.zkane.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import com.zkane.model.AuthUser;
+import com.zkane.repository.AuthUserRepository;
+
+/**
+ * @Description:
+ * @author: 594781919@qq.com
+ * @Date: 2018/4/8 15:26
+ * @version: 1.0
+ */
+@Component
+public class MyUserDetailsService implements UserDetailsService {
+
+	private static final Logger log = LoggerFactory.getLogger(MyUserDetailsService.class);
+
+	@Autowired
+	private AuthUserRepository authUserRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO 这个地方可以通过username从数据库获取正确的用户信息，包括密码和权限等。
+		// List<GrantedAuthority> grantedAuthorityList = new ArrayList<>();
+		// grantedAuthorityList.add(new MyGrantedAuthority("MY_ROLE1",
+		// "MY_MENU1"));
+		// grantedAuthorityList.add(new SimpleGrantedAuthority("ROLE_USER"));
+		// grantedAuthorityList.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+		// return new User(username, "{noop}123456", grantedAuthorityList);
+
+		// 密码在 spring-security-core-5.0.6.RELEASE-sources.jar中的org.springframework.security.authentication.dao.DaoAuthenticationProvider;		
+		// 密码匹配方法 org.springframework.security.crypto.password.DelegatingPasswordEncoder.
+		// 密码的前缀为加密方法，例如：{bcrypt}$2a$10$KkBs8deMrE50KE1v9orMxuBJq6UkROYzAKsDPLo/O.I4vkef0yeMK
+		// spring会按加密方法来验证密码。
+		AuthUser authUser = authUserRepository.findOneByUsername(username);
+		if (authUser == null) {
+			log.error("用户不存在");
+			throw new UsernameNotFoundException(String.format("User % does not exist!", username));
+		}
+		return new MyUserDetails(authUser);
+	}
+}
--- oauth-server/src/test/java/com/zkane/UtilTests.java
+++ oauth-server/src/test/java/com/zkane/UtilTests.java
@@ -2,20 +2,33 @@ package com.zkane;
 
 import org.jasypt.util.text.BasicTextEncryptor;
 import org.junit.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * @author: 594781919@qq.com
  * @date: 2018/4/24
  */
 public class UtilTests {
-    @Test
-    public void jasyptTest() {
-        BasicTextEncryptor encryptor = new BasicTextEncryptor();
-        // application.yml配置的jasypt.encryptor.password
-        encryptor.setPassword("abc123");
-        // 对root进行加密操作
-        System.out.println(encryptor.encrypt("root"));
-        // 进行解密操作
-        System.out.println(encryptor.decrypt("ddc3gpYHXk8o2a9TSXh9Yg=="));
-    }
+	@Test
+	public void jasyptTest() {
+		BasicTextEncryptor encryptor = new BasicTextEncryptor();
+		// application.yml配置的jasypt.encryptor.password
+		encryptor.setPassword("abc123");
+		// 对root进行加密操作
+		System.out.println(encryptor.encrypt("root"));
+		// 进行解密操作
+		System.out.println(encryptor.decrypt("ddc3gpYHXk8o2a9TSXh9Yg=="));
+	}
+
+	/**
+	 * BCryptPasswordEncoder 加密
+	 */
+	@Test
+	public void BCryptPasswordEncoderTest() {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String content = "secret";
+		String head = "{bcrypt}";
+		String encode = encoder.encode(content);
+		System.out.println("BCryptPasswordEncoderTest: [" + content + "] => [" + head + encode + "]");
+	}
 }
